2cb6d818c5b6122b67cff50625a10a6e4bc1cd46
sumi-0011 <selina2000@naver.com>
2023-02-16 21:54:47 +0900

    feat: kakao 액세스 토근 발급 받기

--- src/api/index.ts
+++ src/api/index.ts
@@ -12,4 +12,31 @@ const TOKEN_PATH = KAKAO_HOST + '/oauth/token';
 const login = () => {
   window.location.href = KAKAO_LOGIN_PATH;
 };
+
+export const getToken = async (code: string) => {
+  try {
+    const data = {
+      grant_type: 'authorization_code',
+      client_id: REST_API_KEY,
+      redirect_uri: REDIRECT_URI,
+      code: code,
+    };
+    console.log('data: ', data);
+
+    const res = await axios({
+      method: 'post',
+      url: TOKEN_PATH,
+      data: qs.stringify(data),
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+      },
+    });
+
+    console.log('res: ', res);
+
+    return res.data;
+  } catch (error) {
+    console.log('error: ', error);
+  }
+};
 export default login;
--- src/pages/kakaoLogin.tsx
+++ src/pages/kakaoLogin.tsx
@@ -8,6 +8,23 @@ const getURLparams = (location: Location) => {
   return name;
 };
 
+const tokenSetting = async (code: string) => {
+  const data = await getToken(code);
+  console.log('data: ', data);
+
+  window.location.href = '/';
+  //   {
+  //     "access_token": "kp_JNl4LYdPKq9YSTzSjtUj1fV42s6a7Be_uEnnvCiolUwAAAYZaOZ2Y",
+  //     "token_type": "bearer",
+  //     "refresh_token": "kocQfpkths_8zJhEwDlWdylq25nLpZ7eImrFUHsRCiolUwAAAYZaOZ2W",
+  //     "expires_in": 21599,
+  //     "scope": "account_email profile_image profile_nickname",
+  //     "refresh_token_expires_in": 5183999
+  // }
+  // if (data) {
+  // }
+};
+
 function KakaoLogin() {
   useEffect(() => {
     if (window) {
@@ -15,9 +32,13 @@ function KakaoLogin() {
       if (code) {
         console.log('code: ', code);
         localStorage.setItem('code', code);
+
+        tokenSetting(code);
       }
     }
   }, []);
 
   return <div>KakaoLogin</div>;
 }
+
+export default KakaoLogin;
