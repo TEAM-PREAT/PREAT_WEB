588b4847a2d73e730902da1dbdff1d8cfa75318b
sumi-0011 <selina2000@naver.com>
2023-02-19 21:44:42 +0900

    feat: slider text fix

---
+++ src/styles/font.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const FontSize8 = styled.span`
+  font-size: 10px;
+  transform: scale(0.8);
+`;
+
+export const FontSize5 = styled.span`
+  font-size: 10px;
+  transform: scale(0.5);
+`;
