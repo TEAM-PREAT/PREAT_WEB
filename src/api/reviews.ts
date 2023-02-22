4f507fc2d33342f63a14cdc16d6f1f04f1578734
sumi-0011 <selina2000@naver.com>
2023-02-23 03:29:00 +0900

    feat: review api setting

---
+++ src/api/reviews.ts
@@ -0,0 +1,49 @@
+import { authenticationRequest } from '@/api';
+
+const RESTAURANT_URL = '/v1/restaurants';
+const MY_REVIEWS_URL = '/v1/users/me/reviews';
+
+export const restaurantReviewCreate = async (restaurantId: number) => {
+  const response = await authenticationRequest.post(
+    `${RESTAURANT_URL}/${restaurantId}/reviews`,
+  );
+  if (response.status === 200) {
+    return response.data.data;
+  }
+  throw new Error();
+};
+
+export const restaurantReviewDelete = async (restaurantId: number) => {
+  const response = await authenticationRequest.delete(
+    `${RESTAURANT_URL}/${restaurantId}/reviews`,
+  );
+  if (response.status === 200) {
+    return response.data.data;
+  }
+  throw new Error();
+};
+
+export const restaurantReviewUpdate = async (
+  restaurantId: number,
+  rating: number,
+) => {
+  const response = await authenticationRequest.patch(
+    `${RESTAURANT_URL}/${restaurantId}/reviews`,
+    { rating },
+  );
+  if (response.status === 200) {
+    return response.data.data;
+  }
+  throw new Error();
+};
+
+export const myReviewsDelete = async (idList: number[]) => {
+  const response = await authenticationRequest.patch(MY_REVIEWS_URL, {
+    id: idList,
+  });
+
+  if (response.status === 200) {
+    return response.data.data;
+  }
+  throw new Error();
+};
